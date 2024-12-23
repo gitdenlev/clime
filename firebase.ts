caa6964cfe9b9365cbcb23db93f7472d7d8716e5
Denys <levchenkoden02@gmail.com>
2024-12-23 22:26:44 +0200

    Init project

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCF-aDVgFE3flvh5IIglWXUsIwmy4F24es",
+  authDomain: "clime-a7143.firebaseapp.com",
+  projectId: "clime-a7143",
+  storageBucket: "clime-a7143.firebasestorage.app",
+  messagingSenderId: "266975252291",
+  appId: "1:266975252291:web:e9c753e67e3f07cc3aa9f6",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+console.log(appConfig.theme)
+</script>
+
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <p>Welcome to the home page</p>
+    </div>
+</template>
